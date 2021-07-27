6111a941247dc0c26d41bda1c949989f8ffd71ad
Sadiqwahab <sanchosadiq@gmail.com>
2021-07-27 14:10:56 +0000

    add Contact

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, earum aliquid! Numquam, saepe. Est beatae at debitis dolor nobis totam!</p>
+        </div>
+    );
+}
+
+export default Contact;
