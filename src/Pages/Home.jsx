21004725b50c95e4853a53421c711f371d26e8ac
Sadiqwahab <sanchosadiq@gmail.com>
2021-07-27 14:11:11 +0000

    add Home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, earum aliquid! Numquam, saepe. Est beatae at debitis dolor nobis totam!</p>
+        </div>
+    );
+}
+
+export default Home;
