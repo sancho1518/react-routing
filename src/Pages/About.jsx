58c6cb78ae2d2c041aef5a9bf892ea554f2f3081
Sadiqwahab <sanchosadiq@gmail.com>
2021-07-27 14:10:36 +0000

    add About

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, earum aliquid! Numquam, saepe. Est beatae at debitis dolor nobis totam!</p>
+        </div>
+    );
+}
+
+export default About;
